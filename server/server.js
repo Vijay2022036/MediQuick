44d9e35e1383a72be7ea3658b28f2c6bde67083f
Vijay2022036 <iib2022036@iiita.ac.in>
2025-04-21 00:58:54 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: 'https://medi-quick-vk36.vercel.app', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
