ebadfd1e1af783d0fa4bb47a0707bd033474494a
Vijay2022036 <iib2022036@iiita.ac.in>
2025-04-22 01:51:07 +0530

    tmp

--- client/src/IsLoggedIn.js
+++ client/src/IsLoggedIn.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate , Outlet } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 const isTokenExpired = (token) => {
   try {
