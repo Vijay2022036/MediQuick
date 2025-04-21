bcc4298a9a6e0af248a9594b153e9d48c979d224
Vijay2022036 <iib2022036@iiita.ac.in>
2025-04-22 02:06:37 +0530

    tmp

--- client/src/ProtectedRoute.js
+++ client/src/ProtectedRoute.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+
 
 const ProtectedRoute = ({ allowedRole, fallbackPath }) => {
   const token = localStorage.getItem('token');
   const role = localStorage.getItem('role');
 
   if (!token) {
+    toast.error('You are not authorized to access this page. Please log in.');
     return <Navigate to={fallbackPath} replace />;
   }
 
