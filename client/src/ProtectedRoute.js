e3ccb9f4c22465e0edafc8398ef07c449ab373c7
Vijay2022036 <iib2022036@iiita.ac.in>
2025-04-22 02:07:04 +0530

    tmp

--- client/src/ProtectedRoute.js
+++ client/src/ProtectedRoute.js
@@ -14,6 +14,7 @@ const ProtectedRoute = ({ allowedRole, fallbackPath }) => {
   }
 
   if (allowedRole && allowedRole !== role) {
+    toast.error('You do not have permission to access this page.');
     return <Navigate to={fallbackPath} replace />;
   }
 
